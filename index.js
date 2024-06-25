c399d25a6a24b78510e262d5540fd60ed574a10f
mohamadalhosainy <mohamaddelamyalhosainy28@gmail.com>
2024-06-26 00:01:48 +0300

    fixbug

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const patient = require('./routes/patient_routes/patient_auth_routes');
 const working = require('./routes/medical_routes/WorkingTime');
 const nurseWorking = require('./routes/medical_routes/NurseWorkingTimes');
 const clinical = require('./routes/clinical_examination/clinical_examination_routes');
+const generalInformation = require('./routes/general_information_routes/general_information');
+const othersystem = require('./routes/other_system');
+
 
 
 app.use(express.json()) ;
@@ -25,6 +28,8 @@ app.use('/api/patient' , patient) ;
 app.use('/api/working' , working) ;
 app.use('/api/nurseWorking' , nurseWorking) ;
 app.use('/api/clinical' , clinical) ;
+app.use('/api/generalInformation' , generalInformation) ;
+app.use('/api/othersystem' , othersystem) ;
 
 
 app.listen(3000 , () => console.log('connected')) ;
